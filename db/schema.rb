aee1d5fad35ce50f0e3d2f3535a8e7a2133fd2bd
shandamckinney <mckinney.shanda@gmail.com>
2018-09-30 12:35:17 -0500

    4.2 add styles and colour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_162829) do
+ActiveRecord::Schema.define(version: 2018_09_29_173627) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_162829) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
+    t.string "colour"
   end
 
 end
