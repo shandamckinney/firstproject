70c4ad38cfc59aca6d01433373765fc12d52a87c
shandamckinney <mckinney.shanda@gmail.com>
2018-10-17 12:57:22 -0500

    4.6 orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,16 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders = Order.includes(:product).all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
   end
-  
+
   def create
   end
 
