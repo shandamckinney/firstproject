34f1dc9cdae906bc6d9cbed0dccbf99f72c52a98
shandamckinney <mckinney.shanda@gmail.com>
2018-11-08 11:33:46 -0600

    5.1 redirects and new homepages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,17 +12,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully placed.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  get 'simple_pages/landing_page'
+  root 'products#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
