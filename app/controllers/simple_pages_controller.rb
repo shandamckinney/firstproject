b0acba76975e361afcf23f0ae5bf2edc5699b143
shandamckinney <mckinney.shanda@gmail.com>
2018-11-13 16:05:02 -0600

    5.3 Mailers

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,5 +5,14 @@ class SimplePagesController < ApplicationController
     @featured_product = Product.first
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'mckinney.shanda@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   root 'products#index'
+  post 'simple_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
