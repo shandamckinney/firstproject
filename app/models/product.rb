12454482b8b5d00704eaac7ab7c7b0c8a5fa63c4
shandamckinney <mckinney.shanda@gmail.com>
2018-11-24 14:47:34 -0600

    5.7 added Comment meathod and restyled show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
   def self.search(search_term)
     if Rails.env.production?
 			Product.where("name ilike ?", "%#{search_term}%")
