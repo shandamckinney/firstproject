72081f1d90a16f9d969992f4d920087d3d132d70
shandamckinney <mckinney.shanda@gmail.com>
2018-11-25 16:58:46 -0600

    5.7 added lowest rated comment

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,12 @@ class Product < ApplicationRecord
     comments.rating_desc.first
   end
 
+  def lowest_rating_comment
+		comments.rating_asc.first
+	end
+
+  end
+
   def self.search(search_term)
     if Rails.env.production?
 			Product.where("name ilike ?", "%#{search_term}%")
@@ -13,4 +19,3 @@ class Product < ApplicationRecord
 			Product.where("name LIKE ?", "%#{search_term}%")
 		end
   end
-end
