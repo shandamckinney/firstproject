262c311340e2f4a86bc71a39d101cba94648bf5f
shandamckinney <mckinney.shanda@gmail.com>
2018-12-06 17:21:02 -0600

    5.10 fixed bootstrap

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.nil
+    if user.nil?
       user = User.new
-    elseif user.admin?
+    elsif user.admin?
       can :manage, :all
     else
       can :manage, User, id: user.id
@@ -17,7 +17,7 @@ class Ability
         comment.user_id == user.id
     end
 
-    
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
-  
+
   get 'simple_pages/products'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  get 'simple_pages/landing_page'
-  root 'products#index'
+  root 'simple_pages#landing_page'
+  get 'products/index'
   post 'simple_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
