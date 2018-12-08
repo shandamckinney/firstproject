c3b1c980f3c4d1c6827cc3763391bc5f3212d4a1
shandamckinney <mckinney.shanda@gmail.com>
2018-12-08 08:24:39 -0600

    Added a sign up email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,16 @@ class UserMailer < ApplicationMailer
   default from: "mckinney.shanda@gmail.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(from: email,
          to: 'mckinney.shanda@gmail.com',
          subject: "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
+    @appname = "Bike Shop"
+    mail(to: user.email,
+       subject: "Welcome to #{@appname}!")
   end
 
   def thank_you
