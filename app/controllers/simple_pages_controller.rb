d84f7f2bcfc20d62d6fb4a0308b2c617cd243f8c
shandamckinney <mckinney.shanda@gmail.com>
2018-12-18 11:26:48 -0600

    5.11 final changes

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -9,7 +9,7 @@ class SimplePagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
+    UserMailer.thank_you(@email, @name, @message).deliver_now
   end
 
 end
