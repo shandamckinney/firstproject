386e368979b3777ebae5f4f6126eb7b20bbe4924
shandamckinney <mckinney.shanda@gmail.com>
2019-01-09 11:38:21 -0600

    6.3 changes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,7 +16,13 @@ describe UsersController, type: :controller do
          expect(response).to be_ok
          expect(assigns(:user)).to eq user
        end
-     end 
+
+       it 'cannot access other user accounts' do
+         get :show, params: {id: user_second.id}
+                expect(response).to have_http_status(302)
+                expect(response).to redirect_to root_path
+       end
+     end
 
      context 'when a user is not logged in' do
        it 'redirects to login' do
