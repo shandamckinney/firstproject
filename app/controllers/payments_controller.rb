179b973c10a15b61f70ca1ef8b60cfa86de71841
shandamckinney <mckinney.shanda@gmail.com>
2019-01-11 15:04:19 -0600

    6.3 fixed payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,26 +3,27 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
     def create
-      @product = Product.find(params[:product_id])
-      @user = current_user
-      token = params[:stripeToken]
+      begin
+        @product = Product.find(params[:product_id])
+        @user = current_user
+        token = params[:stripeToken]
 
-        charge = Stripe::Charge.create(
-        amount: (@product.price*100), # amount in cents, again
-        currency: "eur",
-        source: token,
-        description: params[:stripeEmail])
+          charge = Stripe::Charge.create(
+            amount: (@product.price*100), # amount in cents, again
+            currency: "eur",
+            source: token,
+            description: params[:stripeEmail])
 
 
-      if charge.paid
+            if charge.paid
+              Order.create!(:product_id => @product.id, :user_id => @user.id, :total => @product.price)
+            end
 
-        Order.create!(:product_id => @product.id, :user_id => @user.id, :total => @product.price)
-      end
-
-      rescue Stripe::CardError => e
-        body = e.json_body
-        err = body[:error]
-        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]} Your card has not been charged. Please try again."
-      end
-      redirect_to root_path, notice: "Thank you for your purchase."
-    end
+            rescue Stripe::CardError => e
+              body = e.json_body
+              err = body[:error]
+              flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]} Your card has not been charged. Please try again."
+            end
+            redirect_to root_path, notice: "Thank you for your purchase."
+          end
+end
