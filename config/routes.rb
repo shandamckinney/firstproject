6febf4434c3959a3072fe92a05bfed122557d3e4
shandamckinney <mckinney.shanda@gmail.com>
2019-01-17 11:55:10 -0600

    6.12 Redis

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
-
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
