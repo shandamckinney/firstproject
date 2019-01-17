43c7e6e29d74efdda9c4197bbacb3ac5ad4ef048
shandamckinney <mckinney.shanda@gmail.com>
2019-01-17 12:01:46 -0600

    6.13 ActionCable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
 
-
+  config.app_generators.javascript_engine = :javascript
 
   # Do not eager load code on boot.
   config.eager_load = false
