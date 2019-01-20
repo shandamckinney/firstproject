ce629f1426d10dfcc3d593c046f6885a1684c3e5
shandamckinney <mckinney.shanda@gmail.com>
2019-01-20 15:53:42 -0600

    6.13 ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,10 @@ Rails.application.routes.draw do
   get 'products/index'
   post 'simple_pages/thank_you'
 
+  mount ActionCable.server => '/cable'
+  
   resources :orders, only: [:index, :show, :create, :destroy]
 
-  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
